053cfa11e44bbba7e1c52f99566dfe835236dd3a
varonrot <varonrot@gmail.com>
2025-12-12 11:03:13 +0200

    Add files via upload

---
+++ cleanup_earnings_calendar.py
@@ -0,0 +1,15 @@
+import supabase
+import os
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_SERVICE_KEY")
+
+sb = supabase.create_client(url, key)
+
+def main():
+    print("Deleting all rows from earnings_calendar_us ...")
+    sb.table("earnings_calendar_us").delete().neq("symbol", "___")
+    print("Done cleanup.")
+
+if __name__ == "__main__":
+    main()
